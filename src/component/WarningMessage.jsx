86fec4ae474216f8cc5aff42a813586319ae37d3
tomL1985 <tom.lennartz@gmail.com>
2024-04-14 08:47:26 +0200

    git commit

--- src/component/WarningMessage.jsx
+++ src/component/WarningMessage.jsx
@@ -4,7 +4,7 @@ import './Warningmessage.css';
 const WarningMessage = () => {
     return (
         <div className="warning-message">
-            <p>Deze pagina is niet geoptimaliseerd voor gebruik met kleinere devices, gezien ik hier niet voor 💰 krijg ook nog weekend wil vieren 🎉. Bekijk deze pagina op je werklaptop s.v.p.. </p>
+            <p>Deze pagina is niet geoptimaliseerd voor gebruik met kleinere devices, gezien ik hier niet voor 💰 krijg en ook nog weekend wil vieren 🎉. Bekijk deze pagina op je werklaptop s.v.p.. </p>
         </div>
     );
 };
