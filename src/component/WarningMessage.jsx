b23f8ee9aa9c78002740f2a2da8a947aa30833c7
tomL1985 <tom.lennartz@gmail.com>
2024-04-14 11:12:23 +0200

    final commit

--- src/component/WarningMessage.jsx
+++ src/component/WarningMessage.jsx
@@ -7,7 +7,7 @@ function WarningMessage({ closeWarning }) {
             <button className="close-button" onClick={closeWarning}>X</button>
             <div className="warning-message-content">
             <p>Deze pagina is niet volledig geoptimaliseerd voor gebruik met kleinere devices, gezien ik hier niet voor
-                💰 krijg en ook nog weekend wil vieren 🎉. Bekijk deze pagina op je werklaptop s.v.p.. </p>
+                💰 krijg en ook nog weekend wil vieren 🎉. De werklaptop is aanbevolen. </p>
             </div>
         </div>
     );
